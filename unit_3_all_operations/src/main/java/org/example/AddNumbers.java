32841085bdb6e12d5395ee672729583bde7e0171
starchenko <igor.starchenko@gmail.com>
2021-08-07 08:43:01 +0300

    Removed extra println statements

--- unit_3_all_operations/src/main/java/org/example/AddNumbers.java
+++ unit_3_all_operations/src/main/java/org/example/AddNumbers.java
@@ -7,10 +7,12 @@ import java.io.InputStreamReader;
 public class AddNumbers {
     public void run() throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Task 1. Sum of numbers in a string");
+        System.out.println("Type the string:");
         String message = bufferedReader.readLine();
-        System.out.println(message);
+        //System.out.println(message);
         String numbers = message.replaceAll("[^0-9]", "");
-        System.out.println(numbers);
+        //System.out.println(numbers);
         int result = 0;
         char[] chars = numbers.toCharArray();
         for (int i = 0; i < chars.length; i++) {
