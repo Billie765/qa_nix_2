782179f76154e3dccfda3c3ef02a52b6bd6b2673
starchenko <igor.starchenko@gmail.com>
2021-08-07 08:50:36 +0300

    Added the file for the second task

---
+++ unit_3_all_operations/src/main/java/org/example/SortLetters.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class SortLetters {
+    public static void run() throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String message = bufferedReader.readLine();
+        //System.out.println(message);
+        message = message.replaceAll("\\P{L}", "");
+        //System.out.println(message);
+        char[] chars = message.toCharArray();
+        Arrays.sort(chars);
+        //System.out.println(chars);
+        int i = 0;
+        char buffer = chars[0];
+        int total = 0;
+        while (i < chars.length) {
+            if (buffer == chars[i]) {
+                total++;
+            } else {
+                System.out.println(buffer + " - " + total);
+                buffer = chars[i];
+                total = 1;
+            }
+            i++;
+        }
+        System.out.println(buffer + " - " + total);
+    }
+}
