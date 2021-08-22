0393709dfe56b5386f0135843df1476464c97745
starchenko <igor.starchenko@gmail.com>
2021-08-22 21:27:53 +0300

    Added getters and setters

--- unit_5_oop/src/main/java/org/example/entity/Car.java
+++ unit_5_oop/src/main/java/org/example/entity/Car.java
@@ -1,15 +1,42 @@
 package org.example.entity;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
-
-@Getter
-@Setter
-@ToString
 public class Car extends BaseEntity {
 
     private String model;
     private int year;
     private String color;
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "model='" + model + '\'' +
+                ", year=" + year +
+                ", color='" + color + '\'' +
+                ", id=" + getId() +
+                '}';
+    }
 }
