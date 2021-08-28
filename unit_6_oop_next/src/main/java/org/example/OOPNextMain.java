5ca1207429f8bd6a676c07b02034f356ea2b44b7
starchenko <igor.starchenko@gmail.com>
2021-08-28 09:33:16 +0300

    Added main class

---
+++ unit_6_oop_next/src/main/java/org/example/OOPNextMain.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import org.example.service.PlanetReporter;
+
+public class OOPNextMain {
+
+    public static void main(String[] args) {
+        new PlanetReporter().start();
+    }
+}
